775525d8c3ac6ebdf41dd998a4a944485b96d0de
Varun M <varunushamurali@gmail.com>
2025-11-09 22:16:13 +0530

    fix: exported PATH variable

--- dbtinstall.sh
+++ dbtinstall.sh
@@ -4,6 +4,8 @@ apt-get install -y python3.12-venv
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+export PATH=$PATH:/root/.local/bin
+
 uv venv dbtpractice
 
 echo "alias cls='clear'" >>.bashrc
